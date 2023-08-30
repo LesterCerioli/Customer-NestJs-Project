d007cc4aa79801bf4a61bf49984b594e9eb93922
LesterCerioli <lesterlucasit@hotmail.com>
2023-08-30 19:21:10 -0300

    Building Customers API structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/blog'), CustomersModule],
 })
 export class AppModule {}
